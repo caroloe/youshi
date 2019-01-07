7747809a31a22779f622b361ca86626ffd331c62
zhangchun <1209405141@qq.com>
2019-01-07 17:56:06 +0800

    尤氏建设代码

---
+++ app/admin/model/BannerModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangchun
+ * Date: 2019/1/7
+ * Time: 14:32
+ */
+
+namespace app\admin\model;
+
+use think\Model;
+
+class BannerModel extends Model
+{
+
+}
